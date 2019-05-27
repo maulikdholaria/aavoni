19c306a50fe0813021ca0c2e8b54ecc1c7665ee3
Maulik Dholaria <maulik@turo.com>
2019-05-27 01:02:08 -0700

    corrected viewport

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import ThemeStyle from "./style/Theme.less";
 import Header from './screen/common/Header';
-import Viewport from './screen/common/Viewport';
+import ViewPort from './screen/common/ViewPort';
 import HomePage from './screen/HomePage';
 import Maulik from './screen/Maulik';
 import SearchVenue from './screen/SearchVenue';
@@ -15,7 +15,7 @@ function App() {
   return (
   	<BrowserRouter basename="/" >      
   	  <Header></Header>
-  	  <Viewport></Viewport>
+  	  <ViewPort></ViewPort>
       <div>
         <Route path="/" exact={true} component={HomePage} />
         <Route path="/maulik/add/:addid/hello/:helloid" component={Maulik} />
