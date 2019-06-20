6d8d3b5d36bd00feea43e4d1e8548e0c4f9a5a97
maulikdholaria <maulik@turo.com>
2019-06-19 17:42:29 -0700

    Prod (#1)
    * moved dev dependancies
    webpack merge
    
    * moded dependencies
    
    * moved react hot loader to dev dependencies
    
    * ssl certificate
    nginx proxy localhost port connect to back end service
    homepage max width

--- release/bootstrap.sh
+++ release/bootstrap.sh
@@ -2,3 +2,17 @@
 sudo su
 yum install nginx
 systemctl start nginx
+
+
+yum install git
+
+
+mkdir /usr/share/app
+cd /usr/share/app
+
+git clone git@github.com:maulikdholaria/aavoni.git
+
+yum install npm
+
+#For nginx proxy to backend app at port 3001
+setsebool httpd_can_network_connect on -P
---
+++ web-app/webpack.common.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
