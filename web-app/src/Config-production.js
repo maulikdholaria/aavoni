b25968d787e8545f1340763b43cffd4bf8496e92
Maulik Dholaria <maulik@turo.com>
2019-06-19 22:55:54 -0700

    added stripe & google map

---
+++ web-app/src/Config-production.js
@@ -0,0 +1,8 @@
+export const Config = {
+	"stripe": {
+		"PUBLISHABLE_KEY": "pk_live_Ysff5s8DTvmU64LRxcoPJoGk00MW9hsNxm",
+	},
+	"googleMap": {
+		"KEY": "AIzaSyAW9D6T9xGlWAUHlbciguD7QFtjvnPbE8g"
+	}
+}
