aff0cc4afdedb2a09d65bf00da189a80dd7a85ee
Maulik Dholaria <maulik@turo.com>
2019-06-20 19:18:14 -0700

    paused wedding venues for now

--- web-app/src/GlobalMapping.js
+++ web-app/src/GlobalMapping.js
@@ -1,5 +1,5 @@
 export const GlobalMapping = {'modules':[{ value: '1', label: 'Wedding Planner', 'urlParam': 'wedding-planner' },
-									    { value: '2', label: 'Wedding Venue', 'urlParam': 'wedding-venue' },
+									    //{ value: '2', label: 'Wedding Venue', 'urlParam': 'wedding-venue' },
 				 						//{ value: '3', label: 'Corporate Event Planner', 'urlParam': 'corporate-event-planner' },
 										//{ value: '4', label: 'Corporate Event Venue', 'urlParam': 'corporate-event-venue' }
 										],
