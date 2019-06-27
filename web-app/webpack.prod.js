2745d3ded75553a1825d8ee39909b720d815982f
Maulik Dholaria <maulik@turo.com>
2019-06-26 20:13:26 -0700

    added htmkwebpack plugin for separating prod vs dev

--- web-app/webpack.prod.js
+++ web-app/webpack.prod.js
@@ -1,6 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.ejs',
+      filename: 'index.html',
+      templateParameters: {'segmentApiKey': '42CI83ly0QjtNOAOcdYpsOfU71Xjcrrl'}
+    })
+  ],
 });
