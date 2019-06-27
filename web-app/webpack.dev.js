86a31029b51a39c7cf1916537b66ba90a976cd38
maulikdholaria <maulik.dholaria@gmail.com>
2019-06-26 20:50:11 -0700

    added htmkwebpack plugin for separating prod vs dev (#3)
    * added htmkwebpack plugin for separating prod vs dev
    
    * removing hot module from production

--- web-app/webpack.dev.js
+++ web-app/webpack.dev.js
@@ -1,12 +1,18 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.ejs',
+      filename: 'index.html',
+      templateParameters: {'segmentApiKey': 'uoI6xOgFnop6uDJczFsrEoshwBrG3V5T'}
+    })
   ],
   devServer: {
     contentBase: './dist',
