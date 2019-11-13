c1179ca782def12273d8b595994e59e3e95e3513
Maulik Dholaria <maulik@turo.com>
2019-11-13 15:55:57 -0800

    added minified and compression

--- web-app/webpack.prod.js
+++ web-app/webpack.prod.js
@@ -1,6 +1,8 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -11,6 +13,11 @@ module.exports = merge(common, {
       filename: 'index.html',
       templateParameters: {'segmentApiKey': '42CI83ly0QjtNOAOcdYpsOfU71Xjcrrl',
   						   'googleMapsKey': 'AIzaSyAW9D6T9xGlWAUHlbciguD7QFtjvnPbE8g'}
-    })
+    }),
+    new CompressionPlugin({algorithm: 'brotliCompress'})
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 });
